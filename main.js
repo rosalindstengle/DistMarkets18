930ba474dcb3ae08d8be06cb66371dae0f87b977
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-04-22 00:50:57 -0500

    Added chart type selection

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ window.onload = function() {
         window.charts = [];
         document.getElementById('chart_container').innerHTML = '';
     });
+
+    // Changes fields based on chart type
+    $('#chart_type').change(function() {
+        console.log("Shit");
+        $('.query').addClass('hidden');
+        if ($(this).val() == 'all' || $(this).val() == 'commits') {
+            $('#repo_url').removeClass('hidden');
+        }
+        else if ($(this).val() == 'all' || $(this).val() == 'exchange_volume') {
+            $('#currency').removeClass('hidden');
+        }
+    });
 };
 
 function chart_router() {
