e1789185966c7a7d71866a544b3e0e5fcb3dc4cb
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-04-22 00:39:37 -0500

    Refactoring and chart type selection

---
+++ commits_chart.js
@@ -0,0 +1,94 @@
+function load_commits_chart(url) {
+    // Parse URL - we need username and repo name
+    // https://github.com/<username>/<repo>
+    var username = url.split('/')[3];
+    var repo_name = url.split('/')[4];
+
+    // api.github.com/repos/:owner/:repo/stats/contributors
+    $.get("https://api.github.com/repos/" + username + "/" + repo_name + "/stats/contributors", clean_commits_data);
+}
+
+function clean_commits_data(repo_data) {
+    var labels = [];
+    var commits = [];
+    for (var i = 0; i < repo_data.length; i++) {
+        labels.push(repo_data[i].author.login);
+        commits.push(repo_data[i].total);
+    }
+
+    cleaned_data = {
+        labels: labels,
+        datasets: [{
+            label: 'Commits',
+            backgroundColor: window.chartColors.blue,
+            data: commits
+        }]
+    }
+
+    init_commits_chart(cleaned_data);
+}
+
+function get_sample_commits_data() {
+    return {
+        labels: ['Dev A', 'Dev B', 'Dev C', 'Dev D', 'Dev E'],
+        datasets: [{
+            label: 'Additions',
+            backgroundColor: window.chartColors.green,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }, {
+            label: 'Deletions',
+            backgroundColor: window.chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }]
+    };
+}
+
+function init_commits_chart(data) {
+
+    if (data == null) {
+        data = get_sample_commits_data()
+    }
+
+    if (window.charts == undefined) {
+        window.charts = []
+    }
+
+    var new_chart = document.createElement("canvas");
+    var chart_container = document.getElementById("chart_container");
+    chart_container.prepend(new_chart);
+    window.charts.push(new Chart(new_chart.getContext('2d'), {
+        type: 'bar',
+        data: data,
+        options: {
+            title: {
+                display: true,
+                text: 'Developer Commits'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    }));
+}
--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 window.onload = function() {
-    // Loads sample data
-    load_chart();
-
     // Loads data from the repo of the specified URL
     $('#get_data').click(function() {
-        repo_data = get_github_data($('#repo_url').val());
+        chart_router();
     });
 
     // Clears visible charts
@@ -17,99 +14,10 @@ window.onload = function() {
     });
 };
 
-function get_github_data(url) {
-    // Parse URL - we need username and repo name
-    // https://github.com/<username>/<repo>
-    var username = url.split('/')[3];
-    var repo_name = url.split('/')[4];
+function chart_router() {
+    chart_type = $('#chart_type').val();
 
-    // api.github.com/repos/:owner/:repo/stats/contributors
-    $.get("https://api.github.com/repos/" + username + "/" + repo_name + "/stats/contributors", clean_data);
-}
-
-function clean_data(repo_data) {
-    // console.log(repo_data);
-
-    var labels = [];
-    var commits = [];
-    for (var i = 0; i < repo_data.length; i++) {
-        labels.push(repo_data[i].author.login);
-        commits.push(repo_data[i].total);
+    if (chart_type == 'all' || chart_type == 'commits') {
+        load_commits_chart($('#repo_url').val());
     }
-
-    cleaned_data = {
-        labels: labels,
-        datasets: [{
-            label: 'Commits',
-            backgroundColor: window.chartColors.blue,
-            data: commits
-        }]
-    }
-
-    load_chart(cleaned_data);
-}
-
-function get_sample_data() {
-    return {
-        labels: ['Dev A', 'Dev B', 'Dev C', 'Dev D', 'Dev E'],
-        datasets: [{
-            label: 'Additions',
-            backgroundColor: window.chartColors.green,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ]
-        }, {
-            label: 'Deletions',
-            backgroundColor: window.chartColors.red,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ]
-        }]
-    };
-}
-
-function load_chart(data) {
-
-    if (data == null) {
-        data = get_sample_data()
-    }
-
-    if (window.charts == undefined) {
-        window.charts = []
-    }
-
-    var new_chart = document.createElement("canvas");
-    var chart_container = document.getElementById("chart_container");
-    chart_container.prepend(new_chart);
-    window.charts.push(new Chart(new_chart.getContext('2d'), {
-        type: 'bar',
-        data: data,
-        options: {
-            title: {
-                display: true,
-                text: 'Developer Commits'
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false
-            },
-            responsive: true,
-            scales: {
-                xAxes: [{
-                    stacked: true,
-                }],
-                yAxes: [{
-                    stacked: true
-                }]
-            }
-        }
-    }));
 }
