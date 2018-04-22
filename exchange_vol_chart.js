a21780594b605ccdd64c0fe0784c521c3101af04
Roz <rosalind.stengle@gmail.com>
2018-04-22 04:06:33 -0500

    trying not to break stuff

---
+++ exchange_vol_chart.js
@@ -0,0 +1,83 @@
+function load_exchange_volume_chart(currency) {
+
+    jsonfile = $.get("127.0.0.1:5000/volumes/" + currency,
+            clean_exchange_volume_data);
+}
+
+function clean_exchange_volume_data(data) {
+    var exchanges = [];
+    var volumes = [];
+    
+    //json enters here!
+    exchanges = jsonfile.jsonarray.map(function(e) {
+        return e.exchange;
+    });
+    volumes = jsonfile.jsonarray.map(function(e) {
+        return e.volume;
+    });;
+
+    cleaned_data = {
+        labels: exchanges,
+        datasets: [{
+            label: "Exchange Volume",
+            backgroundColor: window.chartColors.blue,
+            data: exchanges
+        }]
+    }
+
+    init_exchange_volume_chart(cleaned_data);
+ }
+
+ function get_sample_exchange_volume_data() {
+    return {
+        labels: ['Exchange A', 'Exchange B', 'Exchange C'],
+        datasets: [{
+            label: "Volume",
+            backgroundColor: window.chartColors.green,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }]
+    };
+}
+
+function init_exchange_volume_chart(data) {
+
+    if (data == null) {
+        data = get_sample_exchange_volume_data()
+    }
+
+    if (window.charts == undefined) {
+        window.charts = []
+    }
+
+    var new_chart = document.createElement("canvas");
+    var chart_container = document.getElementById("chart_container");
+    var char_container = document.prepend(new_chart);
+    windowcharts.push(new Chart(new_chart.getContext('2d'), {
+        type: 'bar',
+        data: data,
+        options: {
+            title: {
+                display: true,
+                text: 'Volume by Exchange'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                }],
+                yAxes: [{
+                    stacked: true,
+                }]
+            }
+        }
+    }));
+}
+
